7f3177a56e3d7a954584daade7843fea3911d681
alehfernandez <alehfernandez@gmail.com>
2023-05-03 17:10:58 -0300

    Tema5 Finalizado

---
+++ src/Tema5/CocheCRUD.java
@@ -0,0 +1,8 @@
+package Tema5;
+
+public interface CocheCRUD {
+
+    void save();
+    void findAll();
+    void delete();
+}
---
+++ src/Tema5/Main.java
@@ -0,0 +1,11 @@
+package Tema5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CocheCRUD cocheCrud = new CocheCRUDImpl();
+        cocheCrud.save();
+        cocheCrud.findAll();
+        cocheCrud.delete();
+    }
+}
