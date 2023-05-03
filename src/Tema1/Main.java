75d8fea7bd271b02ec1ce998da873c39fa4dad12
alehfernandez <alehfernandez@gmail.com>
2023-05-03 16:06:23 -0300

    Tema1 Finalizado

---
+++ src/Tema1/Main.java
@@ -0,0 +1,17 @@
+package Tema1;
+
+public class Main {
+    public static void main(String[] args) {
+        int entero = 10;
+        long enteroLargo = 1000000000L;
+        double decimal = 3.14;
+        boolean booleano = true;
+        String cadena = "Hola, mundo!";
+
+        System.out.println("Valor entero: " + entero);
+        System.out.println("Valor entero largo: " + enteroLargo);
+        System.out.println("Valor decimal: " + decimal);
+        System.out.println("Valor booleano: " + booleano);
+        System.out.println("Valor cadena: " + cadena);
+    }
+}
